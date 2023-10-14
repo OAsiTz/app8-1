369e57f67c4e262bb5abda612b7d3f3834699efa
OAsiTz <110074137+OAsiTz@users.noreply.github.com>
2023-10-14 21:35:55 +0700

    Initial commit

---
+++ local/src/App.js
@@ -0,0 +1,7 @@
+import React from 'react'
+//import RestBasic from './rest-basic'
+//import FormGet from './form-get'
+import FormPost from './form-post'
+export default function App() {
+  return <FormPost/>
+}
